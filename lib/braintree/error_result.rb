3dbc7c153207ff624740c34498d5b7b078c07b58
drew and pg <code@getbraintree.com>
2010-06-24 09:33:31 -0500

    Revert "Revert "[#1837] - provide access to summary on an error response""
    This reverts commit 543b2e327cf1ed5d2c45f61d43252e0b457e6731.

--- lib/braintree/error_result.rb
+++ lib/braintree/error_result.rb
@@ -17,11 +17,12 @@ module Braintree
   #   end
   class ErrorResult
 
-    attr_reader :credit_card_verification, :transaction, :errors, :params
+    attr_reader :credit_card_verification, :transaction, :errors, :params, :summary
 
     def initialize(data) # :nodoc:
       @params = data[:params]
       @credit_card_verification = CreditCardVerification._new(data[:verification]) if data[:verification]
+      @summary = data[:summary]
       @transaction = Transaction._new(data[:transaction]) if data[:transaction]
       @errors = Errors.new(data[:errors])
     end
