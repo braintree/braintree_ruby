f0826580e16dd71e2eb2aec310c1c1d143e53c9d
hammer and patrick <code@getbraintree.com>
2010-07-13 14:45:08 -0500

    [#1904] - Add constants for Verification

--- lib/braintree/credit_card_verification.rb
+++ lib/braintree/credit_card_verification.rb
@@ -2,6 +2,13 @@ module Braintree
   class CreditCardVerification
     include BaseModule
 
+    module Status
+      FAILED = 'failed'
+      GATEWAY_REJECTED = 'gateway_rejected'
+      PROCESSOR_DECLINED = 'processor_declined'
+      VERIFIED = 'verified'
+    end
+
     attr_reader :avs_error_response_code, :avs_postal_code_response_code, :avs_street_address_response_code,
       :cvv_response_code, :merchant_account_id, :processor_response_code, :processor_response_text, :status,
       :gateway_rejection_reason
--- spec/unit/braintree/credit_card_verification_spec.rb
+++ spec/unit/braintree/credit_card_verification_spec.rb
@@ -13,7 +13,23 @@ describe Braintree::CreditCardVerification do
         :processor_response_text => "Do Not Honor",
         :merchant_account_id => "some_id"
       )
+
       verification.inspect.should == %(#<Braintree::CreditCardVerification status: "verified", processor_response_code: "2000", processor_response_text: "Do Not Honor", cvv_response_code: "I", avs_error_response_code: "I", avs_postal_code_response_code: "I", avs_street_address_response_code: "I", merchant_account_id: "some_id", gateway_rejection_reason: nil>)
+    end
+
+    it "has a status" do
+      verification = Braintree::CreditCardVerification._new(
+        :status => "verified",
+        :avs_error_response_code => "I",
+        :avs_postal_code_response_code => "I",
+        :avs_street_address_response_code => "I",
+        :cvv_response_code => "I",
+        :processor_response_code => "2000",
+        :processor_response_text => "Do Not Honor",
+        :merchant_account_id => "some_id"
+      )
+
+      verification.status.should == Braintree::CreditCardVerification::Status::VERIFIED
     end
   end
 end
