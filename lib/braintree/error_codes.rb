11c951682b5ada529cc595a9ca63cb7fae67b900
drew and tony <code@getbraintree.com>
2011-02-14 17:21:42 -0600

    [#2220] - Added error code for subscription modification amount being too large.

--- lib/braintree/error_codes.rb
+++ lib/braintree/error_codes.rb
@@ -121,6 +121,7 @@ module Braintree
       module Modification
         AmountCannotBeBlank = "92003"
         AmountIsInvalid = "92002"
+        AmountIsTooLarge = "92023"
         CannotEditModificationsOnPastDueSubscription = "92022"
         CannotUpdateAndRemove = "92015"
         ExistingIdIsIncorrectKind = "92020"
