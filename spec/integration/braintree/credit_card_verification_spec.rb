d9f2d60fe061ea3ea6046a79b47cd1b6af429865
cosgrove and william <code@getbraintree.com>
2012-11-14 18:08:02 -0600

    experience #7 - adds integration test coverage around card type indicators on verifications

--- spec/integration/braintree/credit_card_verification_spec.rb
+++ spec/integration/braintree/credit_card_verification_spec.rb
@@ -25,5 +25,31 @@ describe Braintree::CreditCardVerification, "search" do
         Braintree::CreditCardVerification.find("invalid-id")
       end.to raise_error(Braintree::NotFoundError, 'verification with id "invalid-id" not found')
     end
+
+    describe "card type indicators" do
+      it "returns prepaid on a prepaid card" do
+        cardholder_name = "Tom #{rand(1_000_000)} Smith"
+
+        result = Braintree::Customer.create(
+          :credit_card => {
+          :cardholder_name => cardholder_name,
+          :expiration_date => "05/2012",
+          :number => Braintree::Test::CreditCardNumbers::CardTypeIndicators::Prepaid,
+          :cvv => '200',
+          :options => {
+            :verify_card => true
+        }
+        })
+
+        search_results = Braintree::CreditCardVerification.search do |search|
+          search.credit_card_cardholder_name.is cardholder_name
+        end
+
+        verification_id = search_results.first.id
+
+        found_verification = Braintree::CreditCardVerification.find(verification_id)
+        found_verification.credit_card[:prepaid].should == Braintree::CreditCard::Prepaid::Yes
+      end
+    end
   end
 end
