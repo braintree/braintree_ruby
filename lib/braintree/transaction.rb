36777697ebbf49331dadfae0c3e1daacffc0d2c7
benhardy and drew <code@getbraintree.com>
2013-05-01 14:34:07 +0000

    Revert "Temporarily remove service fees to release client libraries."
    This reverts commit 4f02ffdc0ff4c2aaea89c5f6bd82097a29b836eb.

--- lib/braintree/transaction.rb
+++ lib/braintree/transaction.rb
@@ -44,7 +44,7 @@ module Braintree
     end
 
     attr_reader :avs_error_response_code, :avs_postal_code_response_code, :avs_street_address_response_code
-    attr_reader :amount, :created_at, :credit_card_details, :customer_details, :subscription_details, :id
+    attr_reader :amount, :created_at, :credit_card_details, :customer_details, :subscription_details, :service_fee_details, :id
     attr_reader :currency_iso_code
     attr_reader :custom_fields
     attr_reader :cvv_response_code
@@ -174,6 +174,7 @@ module Braintree
       set_instance_variables_from_hash(attributes)
       @amount = Util.to_big_decimal(amount)
       @credit_card_details = CreditCardDetails.new(@credit_card)
+      @service_fee_details = ServiceFeeDetails.new(@service_fee)
       @subscription_details = SubscriptionDetails.new(@subscription)
       @customer_details = CustomerDetails.new(@customer)
       @billing_details = AddressDetails.new(@billing)
