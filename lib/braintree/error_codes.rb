336554af2d2ac6af1004d65edd024fc9f7ab7845
cosgrove and pr <code@getbraintree.com>
2013-05-03 18:45:44 +0000

    interruption - Add error code for validation that prevents existing cc token on customer create.

--- lib/braintree/error_codes.rb
+++ lib/braintree/error_codes.rb
@@ -68,6 +68,7 @@ module Braintree
 
       module Options
         UpdateExistingTokenIsInvalid = "91723"
+        UpdateExistingTokenNotAllowed = "91727"
         VerificationMerchantAccountIdIsInvalid = "91728"
       end
     end
