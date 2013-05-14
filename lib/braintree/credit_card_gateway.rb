c0d6ec6a3e54ff53d1324b28d2c8254de3de2012
ben and gary <code@getbraintree.com>
2013-05-14 17:35:53 -0500

    data #228 - Credit cards can be created with security parameters.

--- lib/braintree/credit_card_gateway.rb
+++ lib/braintree/credit_card_gateway.rb
@@ -78,7 +78,7 @@ module Braintree
       billing_address_params = AddressGateway._shared_signature
       options = [:make_default, :verification_merchant_account_id, :verify_card, :venmo_sdk_session]
       signature = [
-        :billing_address_id, :cardholder_name, :cvv, :expiration_date,
+        :billing_address_id, :cardholder_name, :cvv, :device_session_id, :expiration_date,
         :expiration_month, :expiration_year, :number, :token, :venmo_sdk_payment_method_code,
         {:options => options},
         {:billing_address => billing_address_params}
