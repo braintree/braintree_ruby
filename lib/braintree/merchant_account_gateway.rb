c6f2415e038d148ccc6e46d93d7d4c91a105d4c6
adam and chhun <code@getbraintree.com>
2013-05-23 16:52:47 +0000

    marketplace #783 - Verify merchant account create signature.

--- lib/braintree/merchant_account_gateway.rb
+++ lib/braintree/merchant_account_gateway.rb
@@ -6,6 +6,7 @@ module Braintree
     end
 
     def create(attributes)
+      Util.verify_keys(MerchantAccountGateway._create_signature, attributes)
       _do_create "/merchant_accounts/create_via_api", :merchant_account => attributes
     end
 
@@ -16,6 +17,16 @@ module Braintree
       else
         SuccessfulResult.new(:merchant_account => MerchantAccount._new(@gateway, response[:merchant_account]))
       end
+    end
+
+    def self._create_signature # :nodoc:
+      [
+        {:applicant_details => [
+          :first_name, :last_name, :email, :date_of_birth, :ssn, :routing_number, :account_number,
+          {:address => [:street_address, :postal_code, :locality, :region]}]
+        },
+        :tos_accepted, :master_merchant_account_id, :id
+      ]
     end
   end
 end
