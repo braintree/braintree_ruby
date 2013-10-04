59dce0d01e84e09d7c47454606daec91b1635816
brianl and joekur <code@getbraintree.com>
2013-10-04 19:59:06 +0000

    marketplace #1539 - Add the MerchantAccount Update API with initial error codes

---
+++ lib/braintree/merchant_account/funding_details.rb
@@ -0,0 +1,13 @@
+module Braintree
+  class MerchantAccount
+    class FundingDetails
+      include BaseModule
+
+      attr_reader :account_number, :routing_number
+
+      def initialize(attributes)
+        set_instance_variables_from_hash attributes unless attributes.nil?
+      end
+    end
+  end
+end
