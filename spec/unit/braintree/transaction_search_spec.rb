6c50ff198d3056a20214f418eb4e9cfdb8c66b50
brianl and katelyn <code@getbraintree.com>
2013-10-22 19:10:46 +0000

    interruptions #1653 - Switch to an integration test (cherry picked from commit b21a2d96cbb856b0ac4536b3d068a282593e29d9)

--- spec/unit/braintree/transaction_search_spec.rb
+++ spec/unit/braintree/transaction_search_spec.rb
@@ -21,17 +21,4 @@ describe Braintree::TransactionSearch do
       search.billing_company "one"
     end.to raise_error(RuntimeError, "An operator is required")
   end
-
-  context "when a transaction search receives an 422 response" do
-    it "raises a timeout exception" do
-      gateway_mock = stub(:config => OpenStruct.new(:http => true))
-      gateway_mock.config.http.stub(:post).and_return({:unprocessable_entity=>""})
-
-      transaction_gateway = Braintree::TransactionGateway.new(gateway_mock)
-
-      expect {
-        transaction_gateway.search
-      }.to raise_error(Braintree::DownForMaintenanceError)
-    end
-  end
 end
