bfa5d078fe80a8b23c6845841891e285627128b9
adam and pdex <code@getbraintree.com>
2013-11-11 21:32:24 +0000

    marketplace #1539 - Added test for merchant account update.

--- spec/integration/braintree/merchant_account_spec.rb
+++ spec/integration/braintree/merchant_account_spec.rb
@@ -113,4 +113,48 @@ describe Braintree::MerchantAccount do
       result.merchant_account.status.should == Braintree::MerchantAccount::Status::Pending
     end
   end
+
+  describe "update" do
+    it "updates the Merchant Account info" do
+      params = VALID_APPLICATION_PARAMS.clone
+      params.delete(:tos_accepted)
+      params[:individual][:first_name] = "John"
+      params[:individual][:last_name] = "Doe"
+      params[:individual][:email] = "john.doe@example.com"
+      params[:individual][:date_of_birth] = "1970-01-01"
+      params[:individual][:phone] = "3125551234"
+      params[:individual][:address][:street_address] = "123 Fake St"
+      params[:individual][:address][:locality] = "Chicago"
+      params[:individual][:address][:region] = "IL"
+      params[:individual][:address][:postal_code] = "60622"
+      params[:business][:dba_name] = "James's Bloggs"
+      params[:business][:tax_id] = "123456789"
+      params[:funding][:account_number] = "43759348798"
+      params[:funding][:routing_number] = "071000013"
+      result = Braintree::MerchantAccount.update("sandbox_sub_merchant_account", params)
+      result.should be_success
+      result.merchant_account.status.should == "active"
+      result.merchant_account.id.should == "sandbox_sub_merchant_account"
+      result.merchant_account.master_merchant_account.id.should == "sandbox_master_merchant_account"
+      result.merchant_account.individual_details.first_name.should == "John"
+      result.merchant_account.individual_details.last_name.should == "Doe"
+      result.merchant_account.individual_details.email.should == "john.doe@example.com"
+      result.merchant_account.individual_details.date_of_birth.should == "1970-01-01"
+      result.merchant_account.individual_details.phone.should == "3125551234"
+      result.merchant_account.individual_details.address_details.street_address.should == "123 Fake St"
+      result.merchant_account.individual_details.address_details.locality.should == "Chicago"
+      result.merchant_account.individual_details.address_details.region.should == "IL"
+      result.merchant_account.individual_details.address_details.postal_code.should == "60622"
+      result.merchant_account.business_details.dba_name.should == "James's Bloggs"
+    end
+    it "does not require all fields" do
+      result = Braintree::MerchantAccount.update("sandbox_sub_merchant_account", { :individual => { :first_name => "Jose" } })
+      result.should be_success
+    end
+    it "handles unsuccessful results" do
+      result = Braintree::MerchantAccount.update("sandbox_sub_merchant_account", { :individual => { :first_name => "" } })
+      result.should_not be_success
+      result.errors.for(:merchant_account).for(:individual).on(:first_name).first.code.should == Braintree::ErrorCodes::MerchantAccount::Individual::FirstNameIsRequired
+    end
+  end
 end
