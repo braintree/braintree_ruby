64e82d29eb7d3b2c0de02427ac9e876d8399ee38
alex and dcopeland <code@getbraintree.com>
2014-02-07 00:56:18 +0000

    vt KuvNxwfM - Adds limited client-side validation of credit card options when generating a ClientToken.

--- lib/braintree/client_token.rb
+++ lib/braintree/client_token.rb
@@ -3,7 +3,16 @@ require 'json'
 module Braintree
   module ClientToken
     def self.generate(options={})
+      _validate_options(options)
       Configuration.gateway.client_token.generate(options)
+    end
+
+    def self._validate_options(options)
+      [:make_default, :fail_on_duplicate_payment_method, :verify_card].each do |credit_card_option|
+        if options[credit_card_option]
+          raise ArgumentError.new("cannot specify #{credit_card_option} without a customer_id") unless options[:customer_id]
+        end
+      end
     end
   end
 end
--- spec/unit/braintree/client_token_spec.rb
+++ spec/unit/braintree/client_token_spec.rb
@@ -12,5 +12,17 @@ module Braintree
         ClientToken.generate(options)
       end
     end
+
+    context "adding credit_card options with no customer ID" do
+      %w(verify_card fail_on_duplicate_payment_method make_default).each do |option_name|
+        it "raises an ArgumentError if #{option_name} is present" do
+          expect do
+            Braintree::ClientToken.generate(
+              option_name.to_sym => true
+            )
+          end.to raise_error(ArgumentError, /#{option_name}/)
+        end
+      end
+    end
   end
 end
