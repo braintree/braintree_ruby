274dadb15a0049c0dcf3d083b27142730ac6b198
cory and tye <code@getbraintree.com>
2014-02-12 22:03:56 +0000

    marketplace #1627 - Adds inspect to transfer

--- lib/braintree/transfer.rb
+++ lib/braintree/transfer.rb
@@ -2,11 +2,12 @@ module Braintree
   class Transfer
     include BaseModule
 
-    attr_reader :id, :message, :disbursement_date, :follow_up_action, :merchant_account_id
+    attr_reader :amount, :id, :message, :disbursement_date, :follow_up_action, :merchant_account_id
 
     def initialize(gateway, attributes) # :nodoc:
       @gateway = gateway
       set_instance_variables_from_hash(attributes)
+      @amount = Util.to_big_decimal(amount)
     end
 
     def merchant_account
@@ -20,11 +21,28 @@ module Braintree
       end
     end
 
+    def inspect # :nodoc:
+      nice_attributes = self.class._attributes.map do |attr|
+        if attr == :amount
+          "amount: #{self.amount.to_s("F").inspect}"
+        elsif attr == :disbursement_date
+          "disbursement_date: #{self.disbursement_date.to_s}"
+        else
+          "#{attr}: #{send(attr).inspect}"
+        end
+      end
+      "#<#{self.class} #{nice_attributes.join(', ')}>"
+    end
+
     class << self
       protected :new
       def _new(*args) # :nodoc:
         self.new *args
       end
+    end
+
+    def self._attributes # :nodoc:
+      [:id, :amount, :message, :disbursement_date, :follow_up_action]
     end
   end
 end
--- spec/unit/braintree/transfer_spec.rb
+++ spec/unit/braintree/transfer_spec.rb
@@ -8,4 +8,20 @@ describe Braintree::Transfer do
       end.to raise_error(NoMethodError, /protected method .new/)
     end
   end
+
+  describe "inspect" do
+    it "prints attributes of transfer object" do
+      transfer = Braintree::Transfer._new(
+        :gateway,
+        :id => "my_id",
+        :amount => "10.00",
+        :message => "bank_rejected",
+        :disbursement_date => Date.new(2014, 2, 12),
+        :follow_up_action => "update",
+        :merchant_account_id => "token"
+      )
+
+      transfer.inspect.should == '#<Braintree::Transfer id: "my_id", amount: "10.0", message: "bank_rejected", disbursement_date: 2014-02-12, follow_up_action: "update">'
+    end
+  end
 end
