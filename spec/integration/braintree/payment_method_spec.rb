bf64008e51c07719dcdf7d0c3eda761a3899203d
pbcoronel <code@getbraintree.com>
2014-05-09 16:35:53 +0000

    altpay #313 - Add test coverage for paypal accounts to payment_method.create.

--- spec/integration/braintree/payment_method_spec.rb
+++ spec/integration/braintree/payment_method_spec.rb
@@ -70,70 +70,93 @@ describe Braintree::PaymentMethod do
       found_credit_card.should_not be_nil
     end
 
-    it "creates a payment method from an unvalidated paypal account nonce" do
-      with_altpay_merchant do
-        config = Braintree::Configuration.instantiate
-        customer = Braintree::Customer.create.customer
-        client_token = Braintree::ClientToken.generate(:customer_id => customer.id)
-        authorization_fingerprint = JSON.parse(client_token)["authorizationFingerprint"]
-        http = ClientApiHttp.new(
-          config,
-          :authorization_fingerprint => authorization_fingerprint,
-          :shared_customer_identifier => "fake_identifier",
-          :shared_customer_identifier_type => "testing"
-        )
+    context "paypal" do
+      it "creates a payment method from an unvalidated future paypal account nonce" do
+        with_altpay_merchant do
+          config = Braintree::Configuration.instantiate
+          customer = Braintree::Customer.create.customer
+          client_token = Braintree::ClientToken.generate
+          authorization_fingerprint = JSON.parse(client_token)["authorizationFingerprint"]
+          http = ClientApiHttp.new(
+            config,
+            :authorization_fingerprint => authorization_fingerprint,
+          )
 
-        response = http.create_paypal_account(
-          :consent_code => "consent-code",
-          :options => {:validate => false}
-        )
-        response.code.should == "202"
+          response = http.create_paypal_account(
+            :consent_code => "PAYPAL_CONSENT_CODE"
+          )
+          response.code.should == "202"
 
-        nonce = JSON.parse(response.body)["paypalAccounts"].first["nonce"]
-        result = Braintree::PaymentMethod.create(
-          :payment_method_nonce => nonce,
-          :customer_id => customer.id
-        )
+          nonce = JSON.parse(response.body)["paypalAccounts"].first["nonce"]
+          result = Braintree::PaymentMethod.create(
+            :payment_method_nonce => nonce,
+            :customer_id => customer.id
+          )
 
-        result.should be_success
-        result.payment_method.should be_a(Braintree::PayPalAccount)
-        token = result.payment_method.token
+          result.should be_success
+          result.payment_method.should be_a(Braintree::PayPalAccount)
+          token = result.payment_method.token
 
-        found_paypal_account = Braintree::PayPalAccount.find(token)
-        found_paypal_account.should_not be_nil
+          found_paypal_account = Braintree::PayPalAccount.find(token)
+          found_paypal_account.should_not be_nil
+        end
       end
-    end
 
-    it "creates a payment method from a vaulted paypal account" do
-      with_altpay_merchant do
-        config = Braintree::Configuration.instantiate
-        customer = Braintree::Customer.create.customer
-        client_token = Braintree::ClientToken.generate(:customer_id => customer.id)
-        authorization_fingerprint = JSON.parse(client_token)["authorizationFingerprint"]
-        http = ClientApiHttp.new(
-          config,
-          :authorization_fingerprint => authorization_fingerprint,
-          :shared_customer_identifier => "fake_identifier",
-          :shared_customer_identifier_type => "testing"
-        )
+      it "does not create a payment method from an unvalidated onetime paypal account nonce" do
+        with_altpay_merchant do
+          config = Braintree::Configuration.instantiate
+          customer = Braintree::Customer.create.customer
+          client_token = Braintree::ClientToken.generate
+          authorization_fingerprint = JSON.parse(client_token)["authorizationFingerprint"]
+          http = ClientApiHttp.new(
+            config,
+            :authorization_fingerprint => authorization_fingerprint,
+          )
 
-        response = http.create_paypal_account(
-          :consent_code => "consent-code"
-        )
-        response.code.should == "201"
+          response = http.create_paypal_account(
+            :access_token => "PAYPAL_ACCESS_TOKEN",
+          )
+          response.code.should == "202"
 
-        nonce = JSON.parse(response.body)["paypalAccounts"].first["nonce"]
-        result = Braintree::PaymentMethod.create(
-          :payment_method_nonce => nonce,
-          :customer_id => customer.id
-        )
+          nonce = JSON.parse(response.body)["paypalAccounts"].first["nonce"]
+          result = Braintree::PaymentMethod.create(
+            :payment_method_nonce => nonce,
+            :customer_id => customer.id
+          )
 
-        result.should be_success
-        result.payment_method.should be_a(Braintree::PayPalAccount)
-        token = result.payment_method.token
+          result.should_not be_success
+          result.errors.first.message.should == "Consent code is required for vaulting."
+          result.errors.first.code.should == "82902"
+        end
+      end
 
-        found_paypal_account = Braintree::PayPalAccount.find(token)
-        found_paypal_account.should_not be_nil
+      it "returns appropriate validation errors" do
+        with_altpay_merchant do
+          config = Braintree::Configuration.instantiate
+          customer = Braintree::Customer.create.customer
+          client_token = Braintree::ClientToken.generate
+          authorization_fingerprint = JSON.parse(client_token)["authorizationFingerprint"]
+          http = ClientApiHttp.new(
+            config,
+            :authorization_fingerprint => authorization_fingerprint,
+          )
+
+          response = http.create_paypal_account(
+            :token => "PAYPAL_TOKEN",
+          )
+          response.code.should == "202"
+
+          nonce = JSON.parse(response.body)["paypalAccounts"].first["nonce"]
+          result = Braintree::PaymentMethod.create(
+            :payment_method_nonce => nonce,
+            :customer_id => customer.id
+          )
+
+          result.should_not be_success
+          errors = result.errors.map(&:code)
+          errors.should include("82901")
+          errors.should include("82902")
+        end
       end
     end
   end
