9fd04b7294a84f29d5400c4f16f3174cbca7d7b6
brianl <code@getbraintree.com>
2014-05-16 22:16:03 +0000

    altpay #313 - Add test nonces

---
+++ lib/braintree/test/nonce.rb
@@ -0,0 +1,10 @@
+module Braintree
+  module Test
+    module Nonce
+      Transactable = "fake-valid-nonce"
+      Consumed = "fake-consumed-nonce"
+      PayPalOneTimePayment = "fake-paypal-one-time-nonce"
+      PayPalFuturePayment = "fake-paypal-future-nonce"
+    end
+  end
+end
