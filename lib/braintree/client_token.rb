ffc8a20ed778747b0cad0e0ca36857538a8e5fb4
donkuntz and goggin <code@getbraintree.com>
2014-07-01 16:35:43 +0000

    altpay #704 - Don't use magic number for ClientToken default version.

--- lib/braintree/client_token.rb
+++ lib/braintree/client_token.rb
@@ -2,10 +2,12 @@ require 'json'
 
 module Braintree
   module ClientToken
+    DEFAULT_VERSION = 2
+
     def self.generate(options={})
       _validate_options(options)
 
-      options[:version] ||= 2
+      options[:version] ||= DEFAULT_VERSION
 
       Configuration.gateway.client_token.generate(options)
     end
