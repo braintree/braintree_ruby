9c000fd7fea6fdaf560d92c1a4dbc36de2b65079
donkuntz and goggin <code@getbraintree.com>
2014-07-03 19:48:33 +0000

    altpay cleanup - Remove unused code from PaymentMethod.

--- lib/braintree/payment_method.rb
+++ lib/braintree/payment_method.rb
@@ -2,17 +2,6 @@ module Braintree
   class PaymentMethod
     include BaseModule
 
-    attr_reader :email, :token
-
-    def initialize(gateway, attributes) # :nodoc:
-      @gateway = gateway
-      set_instance_variables_from_hash(attributes)
-    end
-
-    def self._new(*args)
-      self.new(*args)
-    end
-
     def self.create(attributes)
       Configuration.gateway.payment_method.create(attributes)
     end
