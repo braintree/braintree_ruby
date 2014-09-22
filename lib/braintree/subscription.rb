dc0e9d3c91f1e611d92dc0dc62edb46f820cd73f
drew <code@getbraintree.com>
2014-09-22 20:19:50 +0000

    interruptions 6db1UNR6 - Expose subscription status events.

--- lib/braintree/subscription.rb
+++ lib/braintree/subscription.rb
@@ -30,6 +30,7 @@ module Braintree
     attr_reader :descriptor
     attr_reader :current_billing_cycle
     attr_reader :updated_at, :created_at
+    attr_reader :status_history
 
     # See http://www.braintreepayments.com/docs/ruby/subscriptions/cancel
     def self.cancel(subscription_id)
@@ -77,6 +78,7 @@ module Braintree
       transactions.map! { |attrs| Transaction._new(gateway, attrs) }
       add_ons.map! { |attrs| AddOn._new(attrs) }
       discounts.map! { |attrs| Discount._new(attrs) }
+      @status_history = attributes[:status_history] ? attributes[:status_history].map { |s| StatusDetails.new(s) } : []
     end
 
     def next_bill_amount
