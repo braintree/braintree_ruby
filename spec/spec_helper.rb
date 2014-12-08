37f84d1b70109f9613d6c87dd710664e20f83f96
alex and linda <code@getbraintree.com>
2014-12-08 23:01:53 +0000

    core zs9WlGLx - Use :should syntax for mocking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -194,4 +194,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expect|
     expect.syntax = [:should, :expect]
   end
+
+  config.mock_with :rspec do |mock|
+    mock.syntax = :should
+  end
 end
