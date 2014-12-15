f6548a2d90892b94700b6eb1d575156d1be4ddee
alechipshear and hurley <code@getbraintree.com>
2014-12-15 21:46:03 +0000

    3ds C9oyAFuM - Create a nonce from a payment method

---
+++ lib/braintree/payment_method_nonce_gateway.rb
@@ -0,0 +1,14 @@
+module Braintree
+  class PaymentMethodNonceGateway # :nodoc:
+    def initialize(gateway)
+      @gateway = gateway
+      @config = gateway.config
+    end
+
+    def create(payment_method_token)
+      response = @config.http.post "/payment_methods/#{payment_method_token}/nonces"
+      payment_method_nonce = PaymentMethodNonce._new(@gateway, response.fetch(:payment_method_nonce))
+      SuccessfulResult.new(:payment_method_nonce => payment_method_nonce)
+    end
+  end
+end
