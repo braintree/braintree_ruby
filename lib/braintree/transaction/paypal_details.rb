1b15686f9ff83ed958b91199c7d13dd08b817dc5
pbcoronel and ryanodonnell <code@getbraintree.com>
2015-02-18 16:52:41 +0000

    pwpp w0mO2lEy - Add capture_id, refund_id, payer_id, payer_last_name, payer_first_name and seller_protection_status to transaction.paypal_details.

--- lib/braintree/transaction/paypal_details.rb
+++ lib/braintree/transaction/paypal_details.rb
@@ -4,7 +4,8 @@ module Braintree
       include BaseModule
 
       attr_reader :custom_field, :payer_email, :payment_id, :authorization_id, :token,
-        :image_url, :debug_id, :payee_email
+        :image_url, :debug_id, :payee_email, :payer_id, :payer_first_name, :payer_last_name,
+        :seller_protection_status, :capture_id, :refund_id
 
       def initialize(attributes)
         set_instance_variables_from_hash attributes unless attributes.nil?
