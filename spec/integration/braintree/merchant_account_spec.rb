b02d66e3a63eb1a3d44e738e62a30a512fbeaeb9
jostendorf and mattpatt <code@getbraintree.com>
2015-06-05 15:35:42 +0000

    Revert "Update MerchantAccount tests to reflect new validation for funding parameters"
    This reverts commit 11b3be7e684d7477471d4a36e8c0cdbce8f970ea.

--- spec/integration/braintree/merchant_account_spec.rb
+++ spec/integration/braintree/merchant_account_spec.rb
@@ -109,7 +109,6 @@ describe Braintree::MerchantAccount do
     context "funding destination" do
       it "accepts a bank" do
         params = VALID_APPLICATION_PARAMS.dup
-        params[:funding] = VALID_APPLICATION_PARAMS[:funding].dup
         params[:funding][:destination] = ::Braintree::MerchantAccount::FundingDestination::Bank
         result = Braintree::MerchantAccount.create(params)
 
@@ -118,9 +117,6 @@ describe Braintree::MerchantAccount do
 
       it "accepts an email" do
         params = VALID_APPLICATION_PARAMS.dup
-        params[:funding] = VALID_APPLICATION_PARAMS[:funding].dup
-        params[:funding].delete(:routing_number)
-        params[:funding].delete(:account_number)
         params[:funding][:destination] = ::Braintree::MerchantAccount::FundingDestination::Email
         params[:funding][:email] = "joebloggs@compuserve.com"
         result = Braintree::MerchantAccount.create(params)
@@ -130,9 +126,6 @@ describe Braintree::MerchantAccount do
 
       it "accepts a mobile_phone" do
         params = VALID_APPLICATION_PARAMS.dup
-        params[:funding] = VALID_APPLICATION_PARAMS[:funding].dup
-        params[:funding].delete(:routing_number)
-        params[:funding].delete(:account_number)
         params[:funding][:destination] = ::Braintree::MerchantAccount::FundingDestination::MobilePhone
         params[:funding][:mobile_phone] = "3125882300"
         result = Braintree::MerchantAccount.create(params)
