c2ebcf69324126fe2c79af70c3820841e1eaee25
jdlc and rod <code@getbraintree.com>
2015-09-22 15:20:35 +0000

    supdev wCRQAov6 - remove parens from method call with no args

--- lib/braintree/payment_method_gateway.rb
+++ lib/braintree/payment_method_gateway.rb
@@ -36,7 +36,7 @@ module Braintree
 
     def delete(token)
       @config.http.delete("#{@config.base_merchant_path}/payment_methods/any/#{token}")
-      SuccessfulResult.new()
+      SuccessfulResult.new
     end
 
     def find(token)
