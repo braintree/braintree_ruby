3c096757cfe4fb26d6a6ba38292770b19591ac09
akowalz and ntomlin <code@getbraintree.com>
2015-11-20 17:03:04 +0000

    Partners AuOQWIpX - Use same logic for client_token and client_token_gateway

--- lib/braintree/client_token.rb
+++ lib/braintree/client_token.rb
@@ -5,19 +5,7 @@ module Braintree
     DEFAULT_VERSION = 2
 
     def self.generate(options={})
-      _validate_options(options)
-
-      options[:version] ||= DEFAULT_VERSION
-
       Configuration.gateway.client_token.generate(options)
-    end
-
-    def self._validate_options(options)
-      [:make_default, :fail_on_duplicate_payment_method, :verify_card].each do |credit_card_option|
-        if options[credit_card_option]
-          raise ArgumentError.new("cannot specify #{credit_card_option} without a customer_id") unless options[:customer_id]
-        end
-      end
     end
   end
 end
