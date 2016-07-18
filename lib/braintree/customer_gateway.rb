295ee1e7ac08ce7a1cb19e8b6b5dd1307d082652
Ivan Tam <ivan.tam@getbraintree.com>
2016-07-18 11:59:11 -0700

    Revert "core 0ySzQBjQ - Allow setting default payment method through Customer…"

--- lib/braintree/customer_gateway.rb
+++ lib/braintree/customer_gateway.rb
@@ -127,11 +127,6 @@ module Braintree
         :company, :email, :fax, :first_name, :id, :last_name, :phone, :website,
         :device_data, :payment_method_nonce, :default_payment_method_token,
         {:credit_card => credit_card_signature},
-        {
-          :payment_method => [
-            {:options => [:update_existing_token, :make_default] }
-          ]
-        },
         {:custom_fields => :_any_key_}
       ]
     end
