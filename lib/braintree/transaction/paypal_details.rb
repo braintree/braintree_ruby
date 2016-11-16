01af8193a11757b5a92942b21fe732aead1f9aa2
abuttars <code@getbraintree.com>
2016-11-16 16:42:04 +0000

    Revert "pwpp w90fLDVb - parsing payer_status from PayPal transactions"
    This reverts commit 8b75bb59f3f9d52c8dfa45b75b7b2da8a6dc3001.

--- lib/braintree/transaction/paypal_details.rb
+++ lib/braintree/transaction/paypal_details.rb
@@ -5,7 +5,7 @@ module Braintree
 
       attr_reader :custom_field, :payer_email, :payment_id, :authorization_id, :token,
         :image_url, :debug_id, :payee_email, :payer_id, :payer_first_name, :payer_last_name,
-        :payer_status, :seller_protection_status, :capture_id, :refund_id, :transaction_fee_amount,
+        :seller_protection_status, :capture_id, :refund_id, :transaction_fee_amount,
         :transaction_fee_currency_iso_code, :description
 
       def initialize(attributes)
