3ac1859d2142de8fc6307dd6577491449098e945
dmerino and lkorth <code@getbraintree.com>
2017-07-12 17:13:31 +0000

    BTDXCLTS-455 - Remove iban bank account from iDEAL webhook

--- spec/unit/braintree/webhook_notification_spec.rb
+++ spec/unit/braintree/webhook_notification_spec.rb
@@ -253,12 +253,6 @@ describe Braintree::WebhookNotification do
         ideal_payment.amount.should == "10.00"
         ideal_payment.approval_url.should == "https://example.com"
         ideal_payment.ideal_transaction_id.should == "1234567890"
-        ideal_payment.iban_bank_account.description.should == "DESCRIPTION ABC"
-        ideal_payment.iban_bank_account.bic.should == "XXXXNLXX"
-        ideal_payment.iban_bank_account.iban_country.should == "11"
-        ideal_payment.iban_bank_account.iban_account_number_last_4.should == "0000"
-        ideal_payment.iban_bank_account.masked_iban.should == "NL************0000"
-        ideal_payment.iban_bank_account.account_holder_name.should == "Account Holder"
       end
 
       it "builds a sample notification for a ideal_payment_failed webhook" do
