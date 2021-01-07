2a76e943cfaebed7fcd2fc00cdad64ec3bf42558
Regis <regis@telnyx.com>
2021-01-07 17:55:26 -0300

    ResourceCollection#first(n)

--- spec/unit/braintree/resource_collection_spec.rb
+++ spec/unit/braintree/resource_collection_spec.rb
@@ -18,6 +18,35 @@ describe "Braintree::ResourceCollection" do
     end
   end
 
+  describe "#first" do
+    it 'returns nil with no results' do
+      values = %w(a b c d e)
+      collection = Braintree::ResourceCollection.new(:search_results => {:ids => [], :page_size => 2}) do |ids|
+        ids.map {|id| values[id] }
+      end
+
+      collection.first.should == nil
+    end
+
+    it 'returns the first occourence' do
+      values = %w(a b c d e)
+      collection = Braintree::ResourceCollection.new(:search_results => {:ids => [0,1,2,3,4], :page_size => 2}) do |ids|
+        ids.map {|id| values[id] }
+      end
+
+      collection.first.should == 'a'
+    end
+
+    it 'returns the first N occourences' do
+      values = %w(a b c d e)
+      collection = Braintree::ResourceCollection.new(:search_results => {:ids => [0,1,2,3,4], :page_size => 2}) do |ids|
+        ids.map {|id| values[id] }
+      end
+
+      collection.first(4).should == ['a','b','c','d']
+    end
+  end
+
   describe "#ids" do
     it "returns a list of the resource collection ids" do
       collection = Braintree::ResourceCollection.new(:search_results => {:ids => [0,1,2,3,4], :page_size => 2})
